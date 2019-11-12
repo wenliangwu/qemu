intel/osadsp-qemu

adsp: host: Add Broxton host side support
Signed-off-by: Liam Girdwood <liam.r.girdwood@linux.intel.com>

hw/i386/acpi-build.c
@@ -2043,6 +2043,7 @@ build_dsdt(GArray *table_data, BIOSLinker *linker,
 
     build_acpi_byt_adsp_devices(dsdt);
     build_acpi_hsw_adsp_devices(dsdt);
+    build_acpi_bxt_adsp_devices(dsdt);
     build_acpi_dwdma_device(dsdt);
 
     if (misc->tpm_version != TPM_VERSION_UNSPEC) {

wenliangwu/qemu

hw/i386/acpi-build.c
@@ -2028,6 +2028,9 @@ build_dsdt(GArray *table_data, BIOSLinker *linker,
     }
 
     build_acpi_byt_adsp_devices(dsdt);
+    build_acpi_hsw_adsp_devices(dsdt);
+    build_acpi_dwdma_device(dsdt);
+
     if (misc->tpm_version != TPM_VERSION_UNSPEC) {
         aml_append(crs, aml_memory32_fixed(TPM_TIS_ADDR_BASE,
                    TPM_TIS_ADDR_SIZE, AML_READ_WRITE));