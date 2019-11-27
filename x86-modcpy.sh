c5fcd13ab0acedea5d8226aed05418b2543931c7
Liam Girdwood <liam.r.girdwood@linux.intel.com>
2019-11-27 12:14:45 +0000

    xtensa: Add script to copy kernel modules to ext4 FS
    Support ext4 kernel updates with helper script to copy kernel modules
    to Poky FS.
    
    Signed-off-by: Liam Girdwood <liam.r.girdwood@linux.intel.com>

---
+++ x86-modcpy.sh
@@ -0,0 +1,37 @@
+# script to copy kernel modules to Poky FS for Qemu
+# Needs to run under sudo for mount and copy to FS
+
+if [ $# -eq 0 ]
+then
+  echo "usage: $0 username"
+  echo "Needs to be run under sudo."
+  exit
+fi
+
+WORK=/home/$1/work
+MOUNT=$WORK/mnt-ext4
+POKY=$WORK/poky/build/tmp/deploy/images
+LINUX=$WORK/linux
+
+echo "check: work dir is $WORK"
+echo "check: mount point is $MOUNT"
+echo "check: poky dir is $POKY"
+echo "check: linux dir is $LINUX"
+echo
+read -p "***  Press enter to continue or CTRL-C to abort ***" var
+
+# Mount ext4 FS as locally
+echo Mounting FS
+mount -o loop $POKY/qemux86-64/core-image-sato-qemux86-64.ext4 $MOUNT
+
+echo Delete old modules....
+rm -fr $MOUNT/lib/modules
+
+# Install modules
+echo install modules
+cd $LINUX
+make modules_install INSTALL_MOD_PATH=$MOUNT
+
+# unount
+umount $MOUNT
+echo Done !
