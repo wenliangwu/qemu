yinyuefengyi/osadsp-qemu

adsp: host: Build host support for Baytrail ADSP
Signed-off-by: Liam Girdwood <liam.r.girdwood@linux.intel.com>

default-configs/i386-softmmu.mak
@@ -58,3 +58,4 @@ CONFIG_IOH3420=y
 CONFIG_I82801B11=y
 CONFIG_SMBIOS=y
 CONFIG_HYPERV_TESTDEV=$(CONFIG_KVM)
+CONFIG_ADSP_HOST=y

wenliangwu/qemu

default-configs/i386-softmmu.mak
@@ -22,6 +22,7 @@
 #CONFIG_TPM_CRB=n
 #CONFIG_TPM_TIS=n
 #CONFIG_VTD=n
+CONFIG_FW_CFG_DMA=y
 
 # Boards:
 #