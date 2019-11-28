1b4c9b1a2633543ab709c964b86e397c5764ec46
Liam Girdwood <liam.r.girdwood@linux.intel.com>
2019-11-28 14:43:07 +0000

    x86-moscopy: update script to copy topologies and firmwares
    Signed-off-by: Liam Girdwood <liam.r.girdwood@linux.intel.com>

--- x86-modcpy.sh
+++ x86-modcpy.sh
@@ -12,11 +12,13 @@ WORK=/home/$1/work
 MOUNT=$WORK/mnt-ext4
 POKY=$WORK/poky/build/tmp/deploy/images
 LINUX=$WORK/linux
+SOF=$WORK/sof/sof
 
 echo "check: work dir is $WORK"
 echo "check: mount point is $MOUNT"
 echo "check: poky dir is $POKY"
 echo "check: linux dir is $LINUX"
+echo "check: sof is $SOF"
 echo
 read -p "***  Press enter to continue or CTRL-C to abort ***" var
 
@@ -26,11 +28,22 @@ mount -o loop $POKY/qemux86-64/core-image-sato-qemux86-64.ext4 $MOUNT
 
 echo Delete old modules....
 rm -fr $MOUNT/lib/modules
+rm -fr $MOUNT/lib/firmware
 
 # Install modules
 echo install modules
 cd $LINUX
 make modules_install INSTALL_MOD_PATH=$MOUNT
+
+# Install Firmware
+echo install firmware
+cd $SOF
+mkdir $MOUNT/lib/firmware/
+mkdir $MOUNT/lib/firmware/intel/
+mkdir $MOUNT/lib/firmware/intel/sof
+mkdir $MOUNT/lib/firmware/intel/sof-tplg
+cp -f `ls build*/*.ri` $MOUNT/lib/firmware/intel/sof
+cp `find . -name *.tplg` $MOUNT/lib/firmware/intel/sof-tplg
 
 # unount
 umount $MOUNT
